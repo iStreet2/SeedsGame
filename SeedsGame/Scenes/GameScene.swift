ac70c30a43f334ca734724d733418f1ddb12920f
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-01-19 18:12:20 -0300

    Função para mexer qualquer nó, testes com um saco de semente, criação das hitBoxes que vão armazenar os números e operandos.

--- SeedsGame/Scenes/GameScene.swift
+++ SeedsGame/Scenes/GameScene.swift
@@ -9,45 +9,86 @@ import Foundation
 import SpriteKit
 
 class GameScene: SKScene {
-	
-	var map: [Int : [String]] = [
-		1: ["x+3=2", "4-4=x-2", "3x+2*8=0"],
-		2: ["paulo", "eh", "legal", "4", "5"],
-		3: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-	]
-	
-	let nextPhaseButton = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
-	
-	
-	override func didMove(to view: SKView) {
-		startup()
-	}
-	
-	
-	func startup() {
-		nextPhaseButton.position = CGPoint(x: 200, y: 100)
-		addChild(nextPhaseButton)
-	}
-	
-	
-	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-		guard let touch = touches.first else { return }
-		
-		if nextPhaseButton.contains(touch.location(in: self)) {
-			print("Before \(GameEngine.shared.currentPhase)")
-			let reveal = SKTransition.reveal(with: .left, duration: 1)
-			print("After \(GameEngine.shared.currentPhase)")
-			
-			scene?.view?.presentScene(GameEngine.shared.phases[GameEngine.shared.currentPhase], transition: reveal)
-		}
-		
-	}
-	
-	
-	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-		
-	}
-	
-	
+    
+    var map: [Int : [String]] = [
+        1: ["x+3=2", "4-4=x-2", "3x+2*8=0"],
+        2: ["paulo", "eh", "legal", "4", "5"],
+        3: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    ]
+        
+    let nextPhaseButton = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+    
+    //Teste para mexer 1 saco de semente
+    let seedBag = SeedBagModel(numero: 3, incognita: false, imageNamed: "seedbag", color: .clear, width: 50, height: 70)
+    var movableNode: SKNode?
+    
+    override func didMove(to view: SKView) {
+        startup()
+    }
+    
+    //Função para mexer algum nó
+    func moveNode(_ node: SKSpriteNode, _ touches: Set<UITouch>, stage: Int){
+        if stage == 0{
+            if let touch = touches.first{
+                let location = touch.location(in: self)
+                if node.contains(location){
+                    movableNode = node
+                    movableNode!.position = location
+                }
+            }
+        }else if stage == 1{
+            if let touch = touches.first {
+                if movableNode != nil{
+                    movableNode!.position = touch.location(in: self)
+                }
+            }
+        }else if stage == 2{
+            if let touch = touches.first {
+                if movableNode != nil{
+                    movableNode!.position = touch.location(in: self)
+                    movableNode = nil
+                }
+            }
+        }
+    }
+    
+    
+    func startup() {
+//        nextPhaseButton.position = CGPoint(x: 200, y: 100)
+//        addChild(nextPhaseButton)
+        
+        //Testes com hitBox
+        seedBag.position = CGPoint(x: 500, y: 100)
+        addChild(seedBag)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //botão trocar de fase
+        guard let touch = touches.first else { return }
+        
+        if nextPhaseButton.contains(touch.location(in: self)) {
+            GameEngine.shared.currentPhase += 1
+            print("Before \(GameEngine.shared.currentPhase)")
+            let reveal = SKTransition.reveal(with: .left, duration: 1)
+            print("After \(GameEngine.shared.currentPhase)")
+            
+            scene?.view?.presentScene(GameEngine.shared.phases[GameEngine.shared.currentPhase], transition: reveal)
+        }
+        
+        //movimento do sprite de semente
+        moveNode(seedBag, touches, stage: 0)
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        moveNode(seedBag, touches, stage: 1)
+    }
+    
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        moveNode(seedBag, touches, stage: 2)
+    }
+    
+    
 }
 
--- SeedsGame/Scenes/PhaseScene.swift
+++ SeedsGame/Scenes/PhaseScene.swift
@@ -11,20 +11,30 @@ import SpriteKit
 class PhaseScene: GameScene {
 	
 	var clients: [ClientModel] = []
+    var hitBoxes: [SKShapeNode] = []
 	
 	
 	init(phase: Int, width: Double, height: Double) {
 		
 		super.init(size: CGSize(width: width, height: height))
 		
-		let clientMap: [Int: Int] = [1: map[1]!.count, 2: map[2]!.count, 3: map[3]!.count]
+		let clientMap: [Int: Int] = [1: map[1]!.count, 2: map[2]!.count, 3: map[3]!.count] //Inicializa as equações dos clientes
 		let eqs = map[phase]
 			
 		for n in 0..<clientMap[phase]! {
 			
 			let client = ClientModel(eqs![n], imageNamed: "Client", color: .blue, size: CGSize(width: 100, height: 100))
+            print(eqs![n])
 			clients.append(client)
 			addChild(client)
+            
+            
+            //Para cada caracter na string da equação, eu crio um quadrado que ira receber sacos dentro dele
+            for _ in eqs![n]{
+                let node = SKShapeNode(rectOf: CGSize(width: 100, height: 100))
+                hitBoxes.append(node)
+            }
+            //PROXIMO PASSO: DIMINUIR O TAMANHO DOS QUADRADOS E CRIAR FUNCAO PARA O SACO GRUDAR NELES! E TAMBEM SEMPRE ACOMPANHAR A QUANTIDADE DE ELEMENTOS DA EQUACAO!!! ACHO QUE TA MOSTRANDO A QUANTIDADE DE QUADRADOS A QUANTIDADE DE CARACTERES DA FASE, NAO DA EQUACAO ATUAL
 		}
 		
 		self.isUserInteractionEnabled = true
@@ -35,14 +45,20 @@ class PhaseScene: GameScene {
 		
 		startup()
 		
-		for (index, client) in clients.enumerated() {
-			client.position = CGPoint(x: 100, y: 100+(150*index))
-			
-			let label = SKLabelNode(text: client.eq)
-			label.position.x = client.position.x
-			label.position.y = client.position.y + 50
-			addChild(label)
-		}
+//		for (index, client) in clients.enumerated() {
+//			client.position = CGPoint(x: 100, y: 100+(150*index))
+//			
+//			let label = SKLabelNode(text: client.eq)
+//			label.position.x = client.position.x
+//			label.position.y = client.position.y + 50
+//			addChild(label)
+//		}
+        
+        for (index,hitBox) in hitBoxes.enumerated(){
+            hitBox.position = CGPoint(x: 100+(150*index), y: 100)
+            hitBox.strokeColor = .red
+            addChild(hitBox)
+        }
 		
 	}
 	
