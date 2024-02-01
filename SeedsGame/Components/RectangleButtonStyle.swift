3a337c6793bd17cda932fb5cd2360f2aea3cd3bb
Dedeti <andrenes2905@gmail.com>
2024-02-01 16:09:23 -0300

    adicionar botões finalizados

---
+++ SeedsGame/Components/RectangleButtonStyle.swift
@@ -0,0 +1,26 @@
+//
+//  SeedButton.swift
+//  SeedsGame
+//
+//  Created by Laura C. Balbachan dos Santos on 24/01/24.
+//
+
+import SwiftUI
+
+struct RectangleButtonStyle: ButtonStyle {
+    var tag: RectangleImages
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .background(configuration.isPressed ? Image(tag.pressed): Image(tag.rawValue))
+            .font(.custom("Troika", size: 32))
+            .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    Button("Infinito") {
+        print("")
+    }
+    .buttonStyle(RectangleButtonStyle(tag: .type1))
+}
